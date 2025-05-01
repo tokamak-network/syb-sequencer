d2336b86e08d678c178b1908b88bddd315d26120
shailu-s <emailid462@gmail.com>
2025-05-01 11:14:34 +0530

    Removed redundant structs

--- sequencer/synchronizer/event_parser.go
+++ sequencer/synchronizer/event_parser.go
@@ -2,63 +2,19 @@ package synchronizer
 
 import (
 	"fmt"
-	"math/big"
 	"strings"
 
 	"github.com/ethereum/go-ethereum/accounts/abi"
-	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/tokamak-network/syb-sequencer/sequencer/abis/bindings"
 )
 
 // Event types
-type ForgeBatchEvent struct {
-	BatchNum     uint32
-	L1UserTxsLen uint32
-	NewLastIdx   uint64
-	NewStateRoot *big.Int
-	NewExitRoot  *big.Int
-	L1UserTxs    []byte
-	Timestamp    uint64
-	ForgerAddr   common.Address
-}
-
 type L1UserTxEvent struct {
 	QueueIndex uint32
 	Position   uint32
 	L1UserTx   []byte
-}
-
-type WithdrawEvent struct {
-	Idx             uint64
-	NumExitRoot     uint32
-	InstantWithdraw bool
-	ToBjj           []byte
-	ToEthAddr       common.Address
-	AmountF         *big.Int
-}
-
-type CreateAccountDepositEvent struct {
-	Idx     uint64
-	EthAddr common.Address
-	AmountF *big.Int
-}
-
-type DepositEvent struct {
-	Idx     uint64
-	AmountF *big.Int
-}
-
-type ExplodeMultipleEvent struct {
-	FromIdx uint64
-	ToIdxs  []uint64
-	AmountF *big.Int
-}
-
-type UnvouchEvent struct {
-	FromIdx uint64
-	ToIdx   uint64
 }
 
 var (
